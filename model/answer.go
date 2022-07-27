d31624ea94c4be9aee7a9d1069031aae8ce8adf3
folson <mr.chenhan344@gmail.com>
2022-07-27 15:50:29 +0800

    refactor: Refactor the model structure

--- model/answer.go
+++ model/answer.go
@@ -1,11 +1,11 @@
 package model
 
+import "gorm.io/gorm"
+
 // Answer 答案表
 type Answer struct {
-	ID      uint   `gorm:"column:id; primaryKey; comment:答案ID"`
+	gorm.Model
 	Content string `gorm:"column:content; comment:答案内容"`
-	Aimg    string `gorm:"column:a_img; comment:答案图片内容"`
-	Atype   uint   `gorm:"column:a_type; size:2; default:0; comment:答案类型默认0选择2内容+图片3图片;"`
-	QID     uint   `gorm:"column:q_id; index; comment:题目ID"`
-	Sort    uint   `gorm:"column:sort; size:7; default:0; comment:排序"`
+	Img     string `gorm:"column:img; comment:答案图片内容"`
+	Type    uint   `gorm:"column:type; type:int; size:2; default:0; comment:答案类型默认0选择2内容+图片3图片;"`
 }
