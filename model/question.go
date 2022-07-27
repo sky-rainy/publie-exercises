61e64cd52eaa2a0a461177a2a49d08a489ae0d74
Raniy <1923021@qq.com>
2022-07-27 14:08:40 +0800

    update the table

--- model/question.go
+++ model/question.go
@@ -5,7 +5,7 @@ type Question struct {
 	ID         uint   `gorm:"column:id; primaryKey; comment:题目ID"`
 	Title      string `gorm:"column:title; size:250; comment:题目名称"`
 	Content    string `gorm:"column:content; size:500; comment:题目内容"`
-	Qimg       string `gorm:"column:q_img; size:500; comment:题目内容"`
+	Qimg       string `gorm:"column:q_img; size:500; comment:题目图片内容"`
 	Qtype      uint   `gorm:"column:q_type; size:7; default:0; comment:题目类型;"`
 	Status     uint   `gorm:"column:status; size:7; default:0; comment:题目状态;"`
 	Sort       uint   `gorm:"column:sort; size:7; comment:排序"`
