9edf4de2c6146599b657635aeb9a422dd31e51fd
Raniy <1923021@qq.com>
2022-07-27 14:06:41 +0800

    update the table

--- model/answer.go
+++ model/answer.go
@@ -4,6 +4,8 @@ package model
 type Answer struct {
 	ID      uint   `gorm:"column:id; primaryKey; comment:答案ID"`
 	Content string `gorm:"column:content; comment:答案内容"`
+	Aimg    string `gorm:"column:a_img; comment:答案内容"`
 	Atype   uint   `gorm:"column:a_type; size:2; default:0; comment:答案类型默认0选择2内容+图片3图片;"`
 	QID     uint   `gorm:"column:q_id; index; comment:题目ID"`
+	Sort    uint   `gorm:"column:sort; size:7; comment:排序"`
 }
--- model/question.go
+++ model/question.go
@@ -5,8 +5,15 @@ type Question struct {
 	ID         uint   `gorm:"column:id; primaryKey; comment:题目ID"`
 	Title      string `gorm:"column:title; size:250; comment:题目名称"`
 	Content    string `gorm:"column:content; size:500; comment:题目内容"`
+	Qimg       string `gorm:"column:q_img; size:500; comment:题目内容"`
 	Qtype      uint   `gorm:"column:q_type; size:7; default:0; comment:题目类型;"`
 	Status     uint   `gorm:"column:status; size:7; default:0; comment:题目状态;"`
+	Sort       uint   `gorm:"column:sort; size:7; comment:排序"`
 	Delete     uint   `gorm:"column:delete; size:7; default:0; comment:题目删除状态;"`
 	CreateTime int    `gorm:"column:create_time; comment:创建时间;"`
+	V1         string `gorm:"column:v1; size:500; comment:选择题1"`
+	V2         string `gorm:"column:v2; size:500; comment:选择题2"`
+	V3         string `gorm:"column:v3; size:500; comment:选择题3"`
+	V4         string `gorm:"column:v4; size:500; comment:选择题4"`
+	V5         string `gorm:"column:v5; size:500; comment:选择题5"`
 }
