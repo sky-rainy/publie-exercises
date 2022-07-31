a9a88ed2a7d932d10d71293fa507a38d1489da50
mashirooooo <chenandmashiro@gmail.com>
2022-08-01 02:01:04 +0800

    处理可能的内存泄露

--- extends/fts/fts.go
+++ extends/fts/fts.go
@@ -13,19 +13,33 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+// go生成的proto文件中的结构体，需要使用unsafe.Pointer来转换
+func createdCBuffer(data []byte) C.Buffer {
+	var buffer C.Buffer
+	buffer.data = (*C.uchar)(unsafe.Pointer(&data[0]))
+	buf_len := len(data)
+	buffer.len = C.ulonglong(buf_len)
+	return buffer
+}
+
+// handleActionResult converts the C buffer to a Go ActionResult
+func handleActionResult(buffer C.Buffer) (*ActionResult, error) {
+	var actionResult ActionResult
+	if err := proto.Unmarshal(*(*[]byte)(unsafe.Pointer(&buffer.data)), &actionResult); err != nil {
+		println("proto.Unmarshal failed:", err)
+		return &actionResult, err
+	}
+	C.free_bytes(buffer)
+	return &actionResult, nil
+}
+
 func Init() C.int {
 	return C.init_fts()
 }
 
 func DeleteAll() (*ActionResult, error) {
 	buffer := C.delete_all()
-	var actionResult ActionResult
-	if err := proto.Unmarshal(*(*[]byte)(unsafe.Pointer(&buffer.data)), &actionResult); err != nil {
-		println("proto.Unmarshal failed:", err)
-		return &actionResult, err
-	}
-	defer C.free_bytes(buffer)
-	return &actionResult, nil
+	return handleActionResult(buffer)
 }
 
 func BatchAdd(contents []*Contents) (*ActionResult, error) {
@@ -38,27 +52,14 @@ func BatchAdd(contents []*Contents) (*ActionResult, error) {
 	if err != nil {
 		return &actionResult, err
 	}
-	var buffer C.Buffer
-	buffer.data = (*C.uchar)(unsafe.Pointer(&contents_bytes[0]))
-	buf_len := len(contents_bytes)
-	buffer.len = C.ulonglong(buf_len)
+	buffer := createdCBuffer(contents_bytes)
 	buffer_rec := C.batch_add(buffer)
-	if err := proto.Unmarshal(*(*[]byte)(unsafe.Pointer(&buffer_rec.data)), &actionResult); err != nil {
-		println("proto.Unmarshal failed:", err)
-		return &actionResult, err
-	}
-	return &actionResult, nil
+	return handleActionResult(buffer_rec)
 }
 
 func Query(query string) (*ActionResult, error) {
 	cquery := C.CString(query)
 	defer C.free(unsafe.Pointer(cquery))
 	buffer := C.query(cquery)
-	var actionResult ActionResult
-	if err := proto.Unmarshal(*(*[]byte)(unsafe.Pointer(&buffer.data)), &actionResult); err != nil {
-		println("proto.Unmarshal failed:", err)
-		return &actionResult, err
-	}
-	defer C.free_bytes(buffer)
-	return &actionResult, nil
+	return handleActionResult(buffer)
 }
